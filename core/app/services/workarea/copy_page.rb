71cf8797d7f279730e5a17144876cea33a76bbc8
Curt Howard <choward@workarea.com>
2020-01-20 15:27:29 -0800

    Add Page copying to Admin
    Modeling Product copying, Page copying allows for a page and its content
    to be cloned and worked on as a separate page.
    
    A deprecation warning has been added to `WORKAREA.productCopyIds` as
    this module was too specific to copying products. The new, generalized
    `WORKAREA.copyIds` should be used instead.
    
    WORKAREA-168

---
+++ core/app/services/workarea/copy_page.rb
@@ -0,0 +1,31 @@
+module Workarea
+  class CopyPage
+    def initialize(page, attrs = {})
+      @page = page
+      @attributes = Workarea.config.page_copy_default_attributes.merge(attrs)
+    end
+
+    def perform
+      page_copy = @page.clone
+      page_copy.assign_attributes(@attributes)
+      page_copy.copied_from = @page
+
+      existing_page = Catalog::Page.find(page_copy.id) rescue nil
+
+      if existing_page.present?
+        page_copy.errors.add(
+          :id,
+          I18n.t('workarea.errors.messages.must_be_unique')
+        )
+      else
+        content_clone = @page.content.clone
+        content_clone.assign_attributes(contentable_id: page_copy.id)
+
+        content_clone.save!
+        page_copy.save!
+      end
+
+      page_copy
+    end
+  end
+end
